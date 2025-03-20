d4f64c2439e57458ea71931a0cc641f1e2b0e5b2
tripfacesmile <98240184+tripfacesmile@users.noreply.github.com>
2025-03-19 21:15:23 -0500

    Update script.js

--- speakeasy-haiku/prototype/script.js
+++ speakeasy-haiku/prototype/script.js
@@ -60,7 +60,9 @@ drone.on('open', error => {
     }
     // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
-    startWebRTC(isOfferer);
+    if (members.length <= 2) {
+      startWebRTC(isOfferer);
+    }
   });
 });
 
